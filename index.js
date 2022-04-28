cd99a7f4e65a7efd2f11e5630e7af4b28bfad55d
ARSiyam <102583517+ARSiyam@users.noreply.github.com>
2022-04-28 20:05:13 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,266 @@
+// const ar=[1,2,34,5,6,65,75,7]
+// const arry=ar.filter(ar => ar <2);
+// console.log(arry)
+
+const product = [
+    {
+        img: 'https://images.unsplash.com/photo-1534217466718-ef4950786e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'Panjabi',
+        Brand: 'Arong',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1610047402714-307d99a677db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'Panjabi',
+        Brand: 'Sultan',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://4.imimg.com/data4/AV/SY/MY-10112154/designer-wedding-sherwani-500x500.jpg',
+        name: 'Panjabi',
+        Brand: 'Star',
+        Price: 2099,
+
+    },
+    {
+        img: 'https://thumbs.dreamstime.com/b/colorful-t-shirts-24939530.jpg',
+        name: 'Shart',
+        Brand: 'Esey',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1598032895397-b9472444bf93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hpcnR8ZW58MHx8MHx8&w=1000&q=80',
+        name: 'Shart',
+        Brand: 'One Point',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf5ekfnEZ-vk_gxTHCYdlqFMvO-aiZnbnV1w&usqp=CAU',
+        name: 'Shart',
+        Brand: 'Lotto',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt8-7DlIs-W9vl_xfeZhm_yImBL1pN1yvIlg&usqp=CAU',
+        name: 'Shart',
+        Brand: 'Estacy',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://i.pinimg.com/236x/ae/1f/33/ae1f33445be907d325564a774ddcc08b.jpg',
+        name: 'Shart',
+        Brand: 'Tex point',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScnBK3k31r3rZvxL2bFEDp5XhvU1TGYIhllw&usqp=CAU',
+        name: 'Shart',
+        Brand: 'Tomy Helipjar',
+        Price: 2000,
+
+    },
+    {
+        img: 'https://media.istockphoto.com/photos/blue-chino-pants-with-brown-leather-belt-isolated-on-white-background-picture-id1149139165?k=20&m=1149139165&s=612x612&w=0&h=GZNt8WgiJ3tSbVmcAKbIUmFAzbulMTw1NJ7msG2Tyno=',
+        name: 'Pent',
+        Brand: 'Esey',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcmbnTjYffj2-KapCIeinMO3mOBOC0K8N067kM1-2bUVxnswt1Pj6-uspMs9ZcDbTg_Lw&usqp=CAU',
+        name: 'Pent',
+        Brand: 'Rox',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://i5.walmartimages.ca/images/Enlarge/987/395/6000198987395.jpg',
+        name: 'Pent',
+        Brand: 'Dainamy',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSnJHeIvAsYkzuKUDhDyCmHhHXqrccKA57nOUp6fIp1pSoMnPM2ey275v6mi7v-adzszM&usqp=CAU',
+        name: 'Pent',
+        Brand: 'Rok Star',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ziAqLA8XwC2M9XdTApel9TGYadFA9O6hyoeVjpKvfanZcQJDZFYaVQRkJ-rj6di71NQ&usqp=CAU',
+        name: 'Pent',
+        Brand: 'Sultan',
+        Price: 2500,
+
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs0JpMNAMav-mAfyBBvekn578-sfdT7SeeAfQzr3VbwUNH4P2nFE_-L0tvFQwJSxpwrO0&usqp=CAU',
+        name: 'Pent',
+        Brand: 'cainij',
+        Price: 2500,
+
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_vQ08M-gIp3BdAJ8UeeJqbVb5_Mn2aQyC4FvS4YUJG50ANQruwELDj85nF0sdg3LHvSE&usqp=CAU',
+        name: 'Pent',
+        Brand: 'Denimy',
+        Price: 2500
+    },
+
+    {
+        img: 'https://thumbs.dreamstime.com/b/boutique-view-interior-34493816.jpg',
+        name: 'butics',
+        Brand: 'Denimy',
+        Price: 4000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzcsaAfd9BP_TCo4bgUb831K-QXp6zd0zPehMisT5RUd8iKxQYljgMu0u2numi-pZAIyQ&usqp=CAU',
+        name: '3ps',
+        Brand: 'Denimy',
+        Price: 4000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP3gY1q81UcPI_7yeWw87lbKyBEAUZmh8LyK5AliAcea55fCwfjrmFVZ5lfBXyEj7d7mQ&usqp=CAU',
+        name: 'sari',
+        Brand: 'tangil',
+        Price: 400
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKsPlEyZd9Tgki6moBYAj6Yj8mtWlYjJTdUY5lb_OWeCY7Qg5Q2MNFipxAE-CpyHTeUgw&usqp=CAU',
+        name: 'kides',
+        Brand: 'Denimy',
+        Price: 2500
+    },
+
+    {
+        img: 'https://5.imimg.com/data5/DF/FS/CN/SELLER-43240710/dsc-6794-jpg-250x250.jpg',
+        name: 'show',
+        Brand: 'Bata',
+        Price: 1000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr2cn96Klm2VxazyVauSUHqdAwWEPfihiMDtGsvbLK_S-A0u7RySeGds5rq5Y8-UGaI5A&usqp=CAU',
+        name: 'show',
+        Brand: 'lotto',
+        Price: 1000
+    },
+
+    {
+        img: 'https://5.imimg.com/data5/WK/OC/WF/SELLER-42319986/boys-shoes-250x250.jpg',
+        name: 'show',
+        Brand: 'nikki',
+        Price: 1000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS2BOExEGkaxkgOZCNhA1VZQBVT1kZovcwxqvbz0LRE6aRmE7uMr6HSdl0ky2vvRSJtVg&usqp=CAU',
+        name: 'show',
+        Brand: 'Apex',
+        Price: 1000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT25WvU7-AU9LmuUV8qlSpTQCSLsv7a9lT4OkQJBx_4Yt_RgXgi25arsnFpSEuJGZrl2d8&usqp=CAU',
+        name: 'show',
+        Brand: 'cainij',
+        Price: 1000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3tgyJ-VkdsSlRCZv6RJxsW_09uWmaH7bo26rASE47mdGZ4QAdFIuyx76qlXOkiDaj72U&usqp=CAU',
+        name: 'show',
+        Brand: 'Bata',
+        Price: 1000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNiVllvOXLbKDLytWJ2qRXStDurpLvXWXm-i5MOKZ4mmS4u1Dt_y20aul0YDUI6ABUWWM&usqp=CAU',
+        name: 'show',
+        Brand: 'lotto',
+        Price: 1000
+    },
+
+    {
+        img: 'https://media.istockphoto.com/photos/sport-shoes-on-isolated-white-background-picture-id956501428?k=20&m=956501428&s=612x612&w=0&h=UC4qdZa2iA0PJvv0RIBlJDyF80wxFyLPq4YWvZa30Sc=',
+        name: 'show',
+        Brand: 'nikki',
+        Price: 2000
+    },
+
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPdOSqcMG2ba4t9LP0-UuXA6Tf-JjSn7BCg0E3BQY3WZVyIxIdEzh31oMONa6oF42ri3c&usqp=CAU',
+        name: 'show',
+        Brand: 'Apex',
+        Price: 2000
+    },
+
+    {
+        img: 'https://image.made-in-china.com/202f0j00bTdfAuQJZyrp/Factory-Men-s-Fashion-Sneakers-Sports-Shoes-Running-Shoes-for-School.jpg',
+        name: 'show',
+        Brand: 'cainij',
+        Price: 2000
+    }
+];
+
+const contener = document.getElementById('card-continer');
+product.map(products => {
+    const div = document.createElement('div');
+    div.classList = 'col';
+    div.innerHTML = `
+    <img src="${products.img}" >
+    <h3>${products.name}</h3>
+    <h4>${products.Brand}</h4>
+    <h2><span>$</span>${products.Price}</h2>
+    <button>Bay</button>
+    `;
+    contener.appendChild(div)
+
+})
+document.getElementById('btn').addEventListener('click', function () {
+    contener.innerHTML = "";
+    const search = document.getElementById('search').value;
+    const serValue = product.filter(products => products.name == search);
+    if (search.length == 0) {
+        product.map(products => {
+            const div = document.createElement('div');
+            div.classList = 'col';
+            div.innerHTML = `
+        <img src="${products.img}" >
+        <h3>${products.name}</h3>
+        <h4>${products.Brand}</h4>
+        <h2><span>$</span>${products.Price}</h2>
+        <button>Bay</button>
+        `;
+            contener.appendChild(div)
+
+        })
+    } else {
+        serValue.map(products => {
+            const div = document.createElement('div');
+            div.classList = 'col';
+            div.innerHTML = `
+    <img src="${products.img}" >
+    <h3>${products.name}</h3>
+    <h4>${products.Brand}</h4>
+    <h2><span>$</span>${products.Price}</h2>
+    <button>Bay</button>
+    `;
+            contener.appendChild(div)
+
+        })
+    }
+
+})
